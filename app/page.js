f9f458e9307e46f19d43facb91b0cc3c822b8f55
Jack Kaloger <jkaloger@users.noreply.github.com>
2024-02-09 12:32:26 +1100

    commit

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get COOL NEW FEATURE started by editing&nbsp;
+          Get COOL NABCEW FEATURE started by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
